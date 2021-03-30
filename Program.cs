32666c55fb541dd06922ef791a0f75c0b6fb9c88
Aubrey-Tsorayi <aubreytsorayi@yahoo.com>
2021-03-30 20:01:43 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPlayer, inputCPU;
+            int randomInt;
+
+            bool playAgain = true;
+
+            while (playAgain == true)
+            {
+                int scorePlayer = 0;
+                int scoreCPU = 0;
+
+                while (scorePlayer < 3 && scoreCPU < 3)
+                {
+
+                    Console.Write("Choose between ROCK, PAPER and SCISSORS: ");
+                    inputPlayer = Console.ReadLine();
+
+                    Random rnd = new Random();
+
+                    randomInt = rnd.Next(1, 4);
+
+                    switch (randomInt)
+                    {
+                        case 1:
+                            inputCPU = "ROCK";
+                            Console.WriteLine("Computer chose ROCK");
+                            if (inputPlayer.ToUpper() == "ROCK")
+                            {
+                                Console.WriteLine("DRAW!\n\n");
+                            }
+                            else if (inputPlayer.ToUpper() == "PAPER")
+                            {
+                                Console.WriteLine("PLAYR WINS!!\n\n");
+                                scorePlayer++;
+                            }
+                            else if (inputPlayer.ToUpper() == "SCISSORS")
+                            {
+                                Console.WriteLine("COMPUTER WINS!!\n\n");
+                                scoreCPU++;
+                            }
+                            break;
+                        case 2:
+                            inputCPU = "PAPER";
+                            Console.WriteLine("Computer chose PAPER");
+                            if (inputPlayer.ToUpper() == "PAPER")
+                            {
+                                Console.WriteLine("DRAW!\n\n");
+                            }
+                            else if (inputPlayer.ToUpper() == "SCISSORS")
+                            {
+                                Console.WriteLine("PLAYR WINS!!\n\n");
+                                scorePlayer++;
+                            }
+                            else if (inputPlayer.ToUpper() == "ROCK")
+                            {
+                                Console.WriteLine("COMPUTER WINS!!\n\n");
+                                scoreCPU++;
+                            }
+                            break;
+                        case 3:
+                            inputCPU = "SCISSORS";
+                            Console.WriteLine("Computer chose SCISSORS");
+                            if (inputPlayer.ToUpper() == "SCISSORS")
+                            {
+                                Console.WriteLine("DRAW!\n\n");
+                            }
+                            else if (inputPlayer.ToUpper() == "ROCK")
+                            {
+                                Console.WriteLine("PLAYR WINS!!\n\n");
+                                scorePlayer++;
+                            }
+                            else if (inputPlayer.ToUpper() == "PAPER")
+                            {
+                                Console.WriteLine("COMPUTER WINS!!\n\n");
+                                scoreCPU++;
+                            }
+                            break;
+                        default:
+                            Console.WriteLine("Invalid entry");
+                            break;
+                    }//switch
+
+                    Console.WriteLine("Scores:\t\tPlayer: {0}\tComputer: {1}", scorePlayer, scoreCPU);
+
+
+                }//while loop
+
+                if (scorePlayer == 3)
+                {
+                    Console.WriteLine("PLAYER WINS!!");
+                }
+                else if (scoreCPU == 3)
+                {
+                    Console.WriteLine("COMPUTER WINS!!");
+                }
+
+                Console.Write("Do you want to play again?(y/n): ");
+                string loop = Console.ReadLine();
+
+                if (loop == "y")
+                {
+                    playAgain = true;
+                    Console.Clear();
+                }
+                else if (loop == "n")
+                {
+                    playAgain = false;
+                }
+                else
+                {
+                    Console.WriteLine("Inalid entry!");
+                }
+
+            }// palyAgain while
+
+            Console.Write("Press enter to continue.......");
+            Console.ReadLine();
+
+        }//main
+    }//class
+}
---
+++ Readme.md
@@ -0,0 +1,15 @@
+<img src = "pic.png" width = "70" height = "70" alt = "Rock, paper and scissors">
+<p><strong>Rock, paper and scissors</strong></p>
+<br>
+<hr>
+
+# Project Name
+ROCK PAPER SCISSORS
+
+## Description
+A simple rock paper and scissor game against the computer.
+
+## What I learned
+1. Using the random class
+2. Use of a switch statement
+3. Use of a nested while loops
